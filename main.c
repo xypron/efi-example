267373df0e499a9f8e87f66a3fe0a8ef45a83ab3
Heinrich Schuchardt <xypron.glpk@gmx.de>
2021-04-01 17:28:09 +0200

    main.c: print arguments
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
-#include <efi.h>
-#include <efilib.h>
+#include <efi/efi.h>
+#include <efi/efilib.h>
+
+static void printargs(EFI_HANDLE ImageHandle)
+{
+	int  argc, i;
+	CHAR16 **argv;
+
+	argc = GetShellArgcArgv(ImageHandle, &argv);
+	if (argc < 1)
+		Print(L"No command line arguments\n");
+	else
+		Print(L"%d command line arguments\n", argc);
+	for (i = 0; i < argc; ++i)
+		Print(L"argv[%d]: %s\n", i, argv[i]);
+}
 
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-   InitializeLib(ImageHandle, SystemTable);
-   Print(L"Hello world, compiled with gnu-efi\n");
 
-   return EFI_SUCCESS;
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"Hello world, compiled with gnu-efi\n");
+
+	printargs(ImageHandle);
+
+	return EFI_SUCCESS;
 }
